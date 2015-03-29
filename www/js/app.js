3e8f2123c7ce5dd6d6111ffde3e18f41fee08ac4
Stephen Coogan <coogie@users.noreply.github.com>
2015-03-29 16:20:14 +0100

    Add rudimentary todo functionality

--- www/js/app.js
+++ www/js/app.js
@@ -3,17 +3,44 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('todo', ['ionic']);
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+app.run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if(window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if(window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
+});
+
+app.controller('TodoCtrl', ['$scope', '$ionicModal', function ($scope, $ionicModal) {
+    $scope.tasks = [];
+
+    $ionicModal.fromTemplateUrl('new-task.html', function (modal) {
+        $scope.taskModal = modal;
+    }, {
+        scope: $scope,
+        animation: 'slide-in-up'
+    });
+
+    $scope.createTask = function (task) {
+        $scope.tasks.push({
+            title: task.title
+        });
+        $scope.taskModal.hide();
+        task.title = "";
+    };
+
+    $scope.newTask = function () {
+        $scope.taskModal.show();
+    };
+
+    $scope.closeNewTask = function () {
+        $scope.taskModal.hide();
+    };
+}]);
