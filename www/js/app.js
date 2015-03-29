1bc811dc9ed793f91abe69d615d7be4a16b0276a
Stephen Coogan <stephen@coog.ie>
2015-03-30 00:39:14 +0100

    Add support for multiple todo projects

--- www/js/app.js
+++ www/js/app.js
@@ -18,29 +18,104 @@ app.run(function($ionicPlatform) {
     });
 });
 
-app.controller('TodoCtrl', ['$scope', '$ionicModal', function ($scope, $ionicModal) {
-    $scope.tasks = [];
+app.controller('TodoCtrl', 
+    ['$scope', '$timeout', '$ionicModal', '$ionicSideMenuDelegate', 'Projects',
+    function ($scope, $timeout, $ionicModal, $ionicSideMenuDelegate, Projects) {
+        var createProject = function(projectTitle) {
+            var newProject = Projects.newProject(projectTitle);
+            $scope.projects.push(newProject);
+            Projects.save($scope.projects);
+            $scope.selectProject(newProject, $scope.projects.length-1);
+        };
 
-    $ionicModal.fromTemplateUrl('new-task.html', function (modal) {
-        $scope.taskModal = modal;
-    }, {
-        scope: $scope,
-        animation: 'slide-in-up'
-    });
+        $scope.projects = Projects.all();
+        
+        $scope.activeProject = $scope.projects[Projects.getLastActiveIndex()];
 
-    $scope.createTask = function (task) {
-        $scope.tasks.push({
-            title: task.title
+        $scope.newProject = function() {
+            var projectTitle = prompt('Project Name');
+            if (projectTitle) {
+                createProject(projectTitle);
+            }
+        };
+
+        $scope.selectProject = function(project, index) {
+            $scope.activeProject = project;
+            Projects.setLastActiveIndex(index);
+            $ionicSideMenuDelegate.toggleLeft(false);
+        };
+
+        $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
+            $scope.taskModal = modal;
+        }, {
+            scope: $scope,
+            animation: 'slide-in-up'
         });
-        $scope.taskModal.hide();
-        task.title = "";
-    };
 
-    $scope.newTask = function () {
-        $scope.taskModal.show();
-    };
+        $scope.createTask = function(task) {
+            if (!$scope.activeProject || !task) {
+                return;
+            }
+            $scope.activeProject.tasks.push({
+                title: task.title
+            });
+            $scope.taskModal.hide();
 
-    $scope.closeNewTask = function () {
-        $scope.taskModal.hide();
-    };
-}]);
+            Projects.save($scope.projects);
+
+            task.title = "";
+        };
+
+        $scope.newTask = function() {
+            $scope.taskModal.show();
+        };
+
+        $scope.closeNewTask = function() {
+            $scope.taskModal.hide();
+        };
+
+        $scope.toggleProjects = function() {
+            $ionicSideMenuDelegate.toggleLeft();
+        };
+
+        $timeout(function() {
+            if ($scope.projects.length == 0) {
+                while (true) {
+                    var projectTitle = prompt('Your first project title:');
+                    if (projectTitle) {
+                        createProject(projectTitle);
+                        break;
+                    }
+                }
+            }
+        });
+    }]
+);
+
+app.factory('Projects', function () {
+    return {
+        all: function() {
+            var projectString = window.localStorage['projects'];
+            
+            if (projectString) {
+                return angular.fromJson(projectString);
+            }
+            return [];
+        },
+        save: function(projects) {
+            window.localStorage['projects'] = angular.toJson(projects);
+        },
+        newProject: function(projectTitle) {
+            return {
+                title: projectTitle,
+                tasks: []
+            }
+        },
+        getLastActiveIndex: function() {
+            return parseInt(window.localStorage['lastActiveProject']) || 0;
+        },
+        setLastActiveIndex: function(index) {
+            window.localStorage['lastActiveProject'] = index;
+        }
+    }
+});
